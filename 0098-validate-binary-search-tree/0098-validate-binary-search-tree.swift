08785313f8dc2cefab1a96049972f19d1a539837
KshitijGaur91 <140344977+KshitijGaur91@users.noreply.github.com>
2023-07-24 08:23:06 +0530

    Time: 46 ms (8.80%), Space: 14.4 MB (99.30%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.swift
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func isValidBST(_ root: TreeNode?) -> Bool {
+       return isValidBSTHelper(root, Int.min, Int.max)
+    }
+    
+    func isValidBSTHelper(_ root: TreeNode?, _ min : Int, _ max : Int) -> Bool {
+        guard let root = root else {
+            return true
+        }
+        if root.val <= min || root.val >= max {
+            return false
+        }
+        return isValidBSTHelper(root.left, min, root.val) && isValidBSTHelper(root.right, root.val, max)
+    }
+}
