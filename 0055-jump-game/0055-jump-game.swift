7be8a0f60f89584604e877af1ae46c3254574759
KshitijGaur91 <140344977+KshitijGaur91@users.noreply.github.com>
2023-10-01 17:19:39 +0530

    Time: 3256 ms (5.08%), Space: 15.1 MB (7.85%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.swift
@@ -0,0 +1,24 @@
+class Solution {
+    func canJump(_ nums: [Int]) -> Bool {
+        var dp = [Int : Bool]()
+        return canJumpHelper(nums, 0, &dp)
+    }
+    
+    func canJumpHelper(_ nums: [Int], _ currentIndex : Int, _ dp : inout [Int : Bool]) -> Bool {
+        if let storedResult = dp[currentIndex] {
+            return storedResult
+        }
+        if currentIndex == nums.count - 1 {
+            return true
+        }
+        if currentIndex >= nums.count {
+            return false
+        }
+        var canJump = false
+        for i in 0..<nums[currentIndex] {
+            canJump = canJump || canJumpHelper(nums, currentIndex + i + 1, &dp)
+        }
+        dp[currentIndex] = canJump
+        return canJump
+    }
+}
