29134ff90dbb66a7a1e8d6ae068ccc388ffa17a1
KshitijGaur91 <140344977+KshitijGaur91@users.noreply.github.com>
2023-10-05 16:35:55 +0530

    Time: 0 ms (100.00%), Space: 13.5 MB (83.14%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.swift
+++ 0062-unique-paths/0062-unique-paths.swift
@@ -1,5 +1,24 @@
 class Solution {
     func uniquePaths(_ m: Int, _ n: Int) -> Int {
+        var dp : [[Int]] = Array(repeating : Array(repeating : 0, count : n), count : m)
+        for i in 0..<m {
+            dp[i][0] = 1
+        }
+        for j in 0..<n {
+            dp[0][j] = 1
+        }
+        for i in 1..<m {
+            for j in 1..<n {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+            }
+        }
+        return dp[m - 1][n - 1]
+    }
+}
+
+/*
+class Solution {
+    func uniquePaths(_ m: Int, _ n: Int) -> Int {
         var map = [String : Int]()
         return uniquePathsHelper(0, 0, &map, m, n)
     }
@@ -19,3 +38,4 @@ class Solution {
         return result
     }
 }
+*/
