899b8e8377d58c3919b7c88766244e9f1653af64
KshitijGaur91 <140344977+KshitijGaur91@users.noreply.github.com>
2023-10-05 20:42:30 +0530

    Time: 1819 ms (5.08%), Space: 19.2 MB (63.85%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.swift
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.swift
@@ -1,24 +1,18 @@
 class Solution {
     func longestConsecutive(_ nums: [Int]) -> Int {
         var numSet = Set(nums)
-        
-        var sequenceStarter = [Int]()
-        for num in nums{
-            if !numSet.contains(num - 1) {
-                sequenceStarter.append(num)
-            }
-        }
-        
         var maxCount = 0
         var currentCount = 0
-        for i in 0..<sequenceStarter.count {
-            var currentNumber = sequenceStarter[i]
-            currentCount = 0
+        for i in 0..<nums.count {
+            var currentNumber = nums[i]
+            if !numSet.contains(currentNumber - 1) {
+                currentCount = 0
             while numSet.contains(currentNumber) {
                 currentCount += 1
                 currentNumber = currentNumber + 1
             }
             maxCount = max(maxCount, currentCount)
+            }
         }
         return maxCount
     }
