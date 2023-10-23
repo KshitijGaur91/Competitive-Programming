6a2c8a44ab23cfa6da541ca3752ec2a2042ace2a
KshitijGaur91 <140344977+KshitijGaur91@users.noreply.github.com>
2023-10-23 14:20:19 +0530

    Time: 57 ms (72.15%), Space: 14.3 MB (58.23%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.swift
+++ 0735-asteroid-collision/0735-asteroid-collision.swift
@@ -2,23 +2,23 @@ class Solution {
     func asteroidCollision(_ asteroids: [Int]) -> [Int] {
         var stack = [Int]()
         for i in 0..<asteroids.count {
-            if asteroids[i] > 0 {
-              stack.append(asteroids[i])  
-            }
-            else {
-                while !stack.isEmpty, stack.last! > 0, abs(asteroids[i]) > stack.last! {
+            let asteroid = asteroids[i]
+            if asteroid > 0 {
+                stack.append(asteroid)
+            } else {
+                while !stack.isEmpty, stack.last! > 0, abs(asteroid) > stack.last! {
                     stack.removeLast()
                 }
-                if !stack.isEmpty, stack.last! > 0, abs(asteroids[i]) == stack.last! {
+                if !stack.isEmpty, abs(asteroid) == stack.last! {
                     stack.removeLast()
+                }
+                else if stack.isEmpty {
+                    stack.append(asteroid)
                 }
                 else if !stack.isEmpty, stack.last! < 0 {
-                    stack.append(asteroids[i])
-                } else if stack.isEmpty {
-                    stack.append(asteroids[i])
+                    stack.append(asteroid)
                 }
             }
-            
         }
         return stack
     }
