a24c8b49635bace84f38e60dfb5c1b1024177891
KshitijGaur91 <140344977+KshitijGaur91@users.noreply.github.com>
2023-11-13 17:21:36 +0530

    Time: 153 ms (38.77%), Space: 14.9 MB (82.09%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.swift
+++ 0283-move-zeroes/0283-move-zeroes.swift
@@ -3,7 +3,9 @@ class Solution {
         var i = 0
         var j = 0
         while j < nums.count {
-            if nums[j] != 0 {
+            if nums[j] == 0 {
+                //j = j + 1
+            } else {
                 nums.swapAt(i, j)
                 i = i + 1
             }
@@ -11,3 +13,7 @@ class Solution {
         }
     }
 }
+
+
+// 0 0 0 1 2 3 0 4 5
+// 1 0 0 0 2 3 0 4 5
