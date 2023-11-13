d1ec7f668e82ede17c8a4a19a90a10f0bf7b7961
KshitijGaur91 <140344977+KshitijGaur91@users.noreply.github.com>
2023-11-13 16:58:57 +0530

    Time: 17 ms (49.75%), Space: 14.1 MB (77.39%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.swift
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.swift
@@ -1,6 +1,27 @@
 class Solution {
     func intersect(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
         var result = [Int]()
+        var arr1 = nums1.sorted()
+        var arr2 = nums2.sorted()
+        var i = 0
+        var j = 0
+        while i < arr1.count, j < arr2.count {
+            if arr1[i] == arr2[j] {
+                result.append(arr1[i])
+                i += 1
+                j += 1
+            } else if arr1[i] < arr2[j] {
+                i += 1
+            } else if arr2[j] < arr1[i] {
+                j += 1
+            }
+        }
+        return result
+    }
+    /*
+    // Map
+    func intersect(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
+        var result = [Int]()
         var freqDict = [Int : Int]()
         for n in nums1 {
             freqDict[n, default : 0] += 1
@@ -13,4 +34,5 @@ class Solution {
         }
         return result
     }
+    */
 }
