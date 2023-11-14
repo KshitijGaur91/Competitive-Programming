46713fc1aeee78a9d05863619a46868984d7bdb5
KshitijGaur91 <140344977+KshitijGaur91@users.noreply.github.com>
2023-11-14 12:00:59 +0530

    Time: 9 ms (86.85%), Space: 16.1 MB (39.45%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.swift
+++ 0125-valid-palindrome/0125-valid-palindrome.swift
@@ -4,10 +4,10 @@ class Solution {
         var i = 0
         var j = strArray.count - 1
         while i < j {
-            while !(strArray[i].isNumber || strArray[i].isLetter), i < j {
+            while !isAlphaNumeric(strArray[i]), i < j {
                 i = i + 1
             }
-            while !(strArray[j].isNumber || strArray[j].isLetter), j > i {
+            while !isAlphaNumeric(strArray[j]), j > i {
                 j = j - 1
             }
             if strArray[i].uppercased() != strArray[j].uppercased() {
@@ -17,5 +17,9 @@ class Solution {
             j = j - 1
         }
         return true
+    }
+    
+    func isAlphaNumeric(_ c : Character) -> Bool {
+        return c.isLetter || c.isNumber
     }
 }
