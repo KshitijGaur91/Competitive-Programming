7d0ff10ded17eace44056c1ca0f1db212ff636bf
KshitijGaur91 <140344977+KshitijGaur91@users.noreply.github.com>
2023-11-14 13:18:52 +0530

    Time: 548 ms (46.42%), Space: 18.2 MB (35.54%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.swift
+++ 0011-container-with-most-water/0011-container-with-most-water.swift
@@ -1,20 +1,18 @@
 class Solution {
     func maxArea(_ height: [Int]) -> Int {
-        var left = 0 
-        var right = height.count - 1
+        var i = 0 
+        var j = height.count - 1
         var maxWater = 0
-        while left < right {
-            let currentWater = min(height[left], height[right]) * (right - left)
-            maxWater = max(maxWater, currentWater)
-            if height[left] < height[right] {
-                left += 1
-            }
-            else if height[right] < height[left] {
-                right -= 1
-            }
-            else {
-                left += 1
-                right -= 1
+        while i < j {
+            let water = min(height[i], height[j]) * (j - i)
+            maxWater = max(maxWater, water)
+            if height[i] < height[j] {
+                i = i + 1
+            } else if  height[j] < height[i] {
+                j = j - 1
+            }else {
+                i = i + 1
+                j = j - 1
             }
         }
         return maxWater
