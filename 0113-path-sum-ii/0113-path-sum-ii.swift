0893d1f70663462600e827fd8922b1a051c80f49
KshitijGaur91 <140344977+KshitijGaur91@users.noreply.github.com>
2023-11-20 19:09:48 +0530

    Time: 25 ms (39.19%), Space: 14.4 MB (74.32%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.swift
+++ 0113-path-sum-ii/0113-path-sum-ii.swift
@@ -16,25 +16,31 @@
 class Solution {
     func pathSum(_ root: TreeNode?, _ targetSum: Int) -> [[Int]] {
         var result = [[Int]]()
-        pathSumHelper(root, targetSum, [], &result)
+        var currentPath = [Int]()
+        pathSumHelper(root, targetSum, &currentPath, &result)
         return result
     }
     
-    func pathSumHelper(_ root: TreeNode?, _ currentSum: Int, _ path : [Int], _ result : inout [[Int]]){
+    func pathSumHelper(_ root: TreeNode?, _ currentSum: Int, _ currentPath : inout [Int], _ result : inout [[Int]]){
         if root == nil {
             return
         }
-        var newPath = path
-        newPath.append(root!.val)
+        currentPath.append(root!.val)
         if root!.left == nil, root!.right == nil {
             if currentSum - root!.val == 0 {
-                result.append(newPath)
+                result.append(currentPath)
                 return
             } else {
                 return
             }
         }
-        pathSumHelper(root?.left, currentSum - root!.val, newPath, &result)
-        pathSumHelper(root?.right, currentSum - root!.val, newPath, &result)
+        pathSumHelper(root?.left, currentSum - root!.val, &currentPath, &result)
+        if root?.left != nil {
+            currentPath.removeLast()
+        }
+        pathSumHelper(root?.right, currentSum - root!.val, &currentPath, &result)
+        if root?.right != nil {
+            currentPath.removeLast()
+        }
     }
 }
