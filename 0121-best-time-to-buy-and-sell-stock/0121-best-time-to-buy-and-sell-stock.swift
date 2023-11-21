8d680d60c805e91711874b9012d1bc443ac960cf
KshitijGaur91 <140344977+KshitijGaur91@users.noreply.github.com>
2023-11-21 10:09:11 +0530

    Time: 744 ms (47.61%), Space: 18 MB (18.73%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.swift
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.swift
@@ -1,8 +1,8 @@
 class Solution {
     func maxProfit(_ prices: [Int]) -> Int {
+        var maxProfit = 0 
         var left = 0
         var right = 1
-        var maxProfit = 0
         while right < prices.count {
             if prices[left] > prices[right] {
                 left = right
